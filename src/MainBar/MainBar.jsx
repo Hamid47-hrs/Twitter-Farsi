271eed1915e4ff8a25c2a904bf66670b4d9242b8
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-26 21:57:04 +0330

    Adjust header in main bar

--- src/MainBar/MainBar.jsx
+++ src/MainBar/MainBar.jsx
@@ -1,11 +1,42 @@
+import { Button, Container, Divider, Grid, TextField, Typography } from "@material-ui/core";
+import { Home, InsertPhotoRounded } from "@material-ui/icons";
 import useStyles from "./MainBarStyle";
-
+import pic from "../images/Hamid.jpg"
 const MainBar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>Main bar</div>
-    )
-}
+  return (
+    <Container className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="stretch"
+      >
+        <Grid item>
+          <Button href="#" className={classes.header}>
+            <Home className={classes.homeIco}/>
+            <Typography className={classes.homeTxt}>خانه</Typography>
+          </Button>
+          <Divider className={classes.divider}/>
+            <div className={classes.fields}>
+                <img src={pic} alt="Profile Picture" className={classes.profilePic}/>
+                <TextField 
+                className={classes.textInput}
+                placeholder="توییت خود را وارد کنید ..."
+                multiline
+                row={3}
+                maxRows={4}/> 
+            </div>
+            <div className={classes.buttons}>
+                <InsertPhotoRounded className={classes.imgIcon}/>
+                <Button variant="outlined" color="primary" className={classes.tweetButton}>توییت کن</Button>
+            </div>
+        </Grid>
+        <Grid item className={classes.body}></Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default MainBar;
--- src/MainBar/MainBarStyle.js
+++ src/MainBar/MainBarStyle.js
@@ -2,8 +2,56 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "green",
+    backgroundColor: "lightgreen",
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
   },
+  header: {
+    margin: "0.5rem 0",
+    alignItems: "flex-end",
+    "&:hover": {
+      color: theme.palette.action.active,
+    }
+  },
+  homeIco: {
+    fontSize: "2rem",
+    marginLeft: theme.spacing(1),
+
+  },
+  divider: {
+    width: "100%",
+    backgroundColor: "#aaa",
+    marginBottom: theme.spacing(2),
+  },
+  fields: {
+    display: "flex",
+    alignItems: "center",
+  },
+  profilePic: {
+    width: "6rem",
+    borderRadius: "50%",
+    marginLeft: theme.spacing(2),
+  },
+  textInput: {
+    flex: 1,
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  imgIcon: {
+    marginBottom: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    fontSize: "1.7rem",
+    color: theme.palette.secondary.main,
+    "&:hover": {
+      color: theme.palette.secondary.dark,
+    }
+  },
+  tweetButton: {
+    marginBottom: theme.spacing(2),
+  }
 }));
 
 export default useStyles;
