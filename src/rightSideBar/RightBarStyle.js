4f9608c55df72ce95b9f16fafcb3039c1bc12505
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-26 20:49:32 +0330

    Design right side bar with full style

--- src/rightSideBar/RightBarStyle.js
+++ src/rightSideBar/RightBarStyle.js
@@ -1,8 +1,76 @@
 import { makeStyles } from "@material-ui/core";
+import { blue, red, yellow } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "red",
+    backgroundColor: yellow[200],
+    padding: "0.2rem",
+    paddingTop: theme.spacing(3),
+  },
+  header: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing(5),
+  },
+  logo: {
+    width: "4rem",
+    filter:
+      "invert(50%) sepia(90%) hue-rotate(140deg) saturate(4) brightness(90%)",
+    marginLeft: theme.spacing(1),
+    [theme.breakpoints.down("sm")]: {
+      width: "40%",
+    },
+  },
+  title1: {
+    color: "#318bf3",
+    fontWeight: 700,
+    [theme.breakpoints.down("770")]: {
+      display: "none",
+    },
+  },
+  title2: {
+    fontSize: "130%",
+    color: "#318bf3",
+    fontWeight: 700,
+    [theme.breakpoints.up("770")]: {
+      display: "none",
+    },
+  },
+  hashtag: {
+    fontWeight: 600,
+    fontSize: "1.3rem",
+    textAlign: "center",
+    textShadow: "0 0 2px #aaa",
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.down("860")]: {
+      fontSize: "1rem",
+    },
+  },
+  hashLink: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "0.5rem",
+    "&:hover": {
+      backgroundColor: "#315dee",
+      color: "#fff",
+    },
+  },
+  hashLogo: {
+    width: "2rem",
+    marginLeft: theme.spacing(2),
+    border: "1px solid #315dee",
+    padding: "0.2rem",
+    borderRadius: "4px",
+    [theme.breakpoints.down("425")]: {
+      width: "30%",
+    },
+  },
+  TagText: {
+    fontSize: "1.1rem",
+    [theme.breakpoints.down("425")]: {
+      fontSize: "0.7rem",
+    },
   },
 }));
 
--- src/rightSideBar/RightSideBar.jsx
+++ src/rightSideBar/RightSideBar.jsx
@@ -1,11 +1,70 @@
+import logo from "../images/Butterfly.svg";
+import Hashtag from "../images/HashTag.png";
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  Button,
+} from "@material-ui/core";
 import useStyles from "./RightBarStyle";
 
 const RightSideBar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>right side bar</div>
-    )
-}
+  return (
+    <Container className={classes.root}>
+      <div className={classes.header}>
+        <img src={logo} alt="logo" className={classes.logo} />
+        <Typography className={classes.title1} variant="h5" component="h1">
+          توییتر فارسی
+        </Typography>
+        <Typography className={classes.title2} variant="h5" component="h1">
+          توییتر
+        </Typography>
+      </div>
+      <div>
+        <Typography className={classes.hashtag}>داغ ترین هشتگ ها</Typography>
+        <div>
+          <Button href="#" className={classes.hashLink}>
+            <img
+              src={Hashtag}
+              alt="HashTag-Logo"
+              className={classes.hashLogo}
+            />
+            <Typography className={classes.TagText}>هشتگ_اول</Typography>
+          </Button>
+          <Button href="#" className={classes.hashLink}>
+            <img
+              src={Hashtag}
+              alt="HashTag-Logo"
+              className={classes.hashLogo}
+            />
+            <Typography className={classes.TagText}>هشتگ_اول</Typography>
+          </Button>
+          <Button href="#" className={classes.hashLink}>
+            <img
+              src={Hashtag}
+              alt="HashTag-Logo"
+              className={classes.hashLogo}
+            />
+            <Typography className={classes.TagText}>هشتگ_اول</Typography>
+          </Button>
+          <Button href="#" className={classes.hashLink}>
+            <img
+              src={Hashtag}
+              alt="HashTag-Logo"
+              className={classes.hashLogo}
+            />
+            <Typography className={classes.TagText}>هشتگ_اول</Typography>
+          </Button>
+          
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default RightSideBar;
