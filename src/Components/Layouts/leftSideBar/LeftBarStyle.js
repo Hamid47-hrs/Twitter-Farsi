fcdf8d2ebc5e559fe04044de45608296e0ed9d72
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-28 19:27:22 +0330

    Adjust left side bar with full style

--- src/Components/Layouts/leftSideBar/LeftBarStyle.js
+++ src/Components/Layouts/leftSideBar/LeftBarStyle.js
@@ -1,8 +1,45 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: "blue",
+  root: {},
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "start",
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+  profilePicture: {
+    width: "40%",
+    borderRadius: "50%",
+    marginLeft: theme.spacing(2),
+    boxShadow: "0 0 10px #aaa",
+  },
+  userName: {
+    color: "#aaa",
+  },
+  users: {
+    width: "100%",
+    marginTop: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
+  },
+  userImage: {
+    width: "50px",
+    borderRadius: "50%",
+    marginLeft: theme.spacing(2),
+  },
+  buttonContainer: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  userNames: {
+    fontSize: "0.9rem",
+    textTransform: "capitalize",
+  },
+  userID: {
+    fontSize: "0.9rem",
+    textTransform: "capitalize",
+    color: "#aaa",
   },
 }));
 
--- src/Components/Layouts/leftSideBar/LeftSideBar.jsx
+++ src/Components/Layouts/leftSideBar/LeftSideBar.jsx
@@ -1,11 +1,87 @@
+import { Button, Container, Divider, Grid, Typography } from "@material-ui/core";
 import useStyles from "./LeftBarStyle";
-
+import pic from "../../../images/Hamid.jpg"
 const LeftSideBar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>left side bar</div>
-    )
-}
+  return (
+    <Container className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-start"
+      >
+          <Grid item className={classes.header}>
+              <img className={classes.profilePicture} src={pic} alt="prifile pic"/>
+              <div>
+                  <Typography>حمید رضا</Typography>
+                  <Typography className={classes.userName}>HamidReza@</Typography>
+              </div>
+          </Grid>
+          <Divider />
+          <Grid item>
+              <div className={classes.buttonContainer}>
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  <Divider />
+                  <Button href="#" className={classes.users}>
+                      <img className={classes.userImage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJd6FLxNV-xGNmMkIIDxyUFin0zE2TMa-bkA&usqp=CAU" alt="jim"/>
+                      <div>
+                          <Typography className={classes.userNames}>Jim Cari</Typography>
+                          <Typography className={classes.userID}>jim4carrey@</Typography>
+                      </div>
+                  </Button>
+                  
+              </div>
+          </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default LeftSideBar;
