f1a2080ff8faa71b217f24beb7cb7d0a4fc74260
Hamid.hrs <hamid47.hrs@gmail.com>
2021-12-31 22:49:39 +0330

    Change the App component to parent for route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import { Fragment } from "react";
+import { Grid } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RightSideBar from "./Components/Layouts/rightSideBar/RightSideBar";
 import MainBar from "./Components/Layouts/MainBar/MainBar";
 import LeftSideBar from "./Components/Layouts/leftSideBar/LeftSideBar";
-import { Grid } from "@material-ui/core";
+import HomePage from "./Components/Layouts/HomePage/HomePage";
+import TweetByUser from "./Components/Layouts/Pages/Users/TweetByUser";
+import Page404 from "./Components/Layouts/Pages/Page404/Page404";
 
 const App = () => {
   return (
     <Fragment>
-      <Grid container>
-        <Grid item sm={2} xs={3}>
-          <RightSideBar />
+      <Router>
+        <Grid container>
+          <Grid item sm={2} xs={3}>
+            <RightSideBar />
+          </Grid>
+          <Grid item sm={7} xs={9}>
+            <MainBar>
+              <Switch>
+                <Route exact path={"/"} component={HomePage} />
+                <Route path={"/jim_carrey"} component={TweetByUser} />
+                <Route component={Page404} />
+              </Switch>
+            </MainBar>
+          </Grid>
+          <Grid item sm={3}>
+            <LeftSideBar />
+          </Grid>
         </Grid>
-        <Grid item sm={7} xs={9}>
-          <MainBar />
-        </Grid>
-        <Grid item sm={3}>
-          <LeftSideBar />
-        </Grid>
-      </Grid>
+      </Router>
     </Fragment>
   );
-}
+};
 
 export default App;
--- src/Components/Layouts/MainBar/MainBar.jsx
+++ src/Components/Layouts/MainBar/MainBar.jsx
@@ -1,23 +1,10 @@
 import { Container } from "@material-ui/core";
 import useStyles from "./MainBarStyle";
-import HomePage from "../HomePage/HomePage";
-import TweetByUser from "../Pages/Users/TweetByUser";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 
-const MainBar = () => {
+const MainBar = (props) => {
   const classes = useStyles();
 
-  return (
-    <Container className={classes.root}>
-      <Router>
-        <Link to="jim_carrey">jim carrey</Link>
-        <Switch>
-          <Route exact path={"/"} component={HomePage} />
-          <Route path={"/jim_carrey"} component={TweetByUser} />
-        </Switch>
-      </Router>
-    </Container>
-  );
+  return <Container className={classes.root}>{props.children}</Container>;
 };
 
 export default MainBar;
