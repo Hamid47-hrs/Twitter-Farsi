a5286ee28f917b44351c6f351b04cbebb6f21a12
Hamid.hrs <hamid47.hrs@gmail.com>
2022-01-15 23:13:08 +0330

    Adjust like fetch API

--- src/Components/Layouts/MainBar/TweetList.jsx
+++ src/Components/Layouts/MainBar/TweetList.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Grid, Typography, Button } from "@material-ui/core";
 import { Favorite, Repeat } from "@material-ui/icons";
 import pic from "../../../images/BlankProfile.png";
 import useStyles from "./TweetListStyle";
-import {
-  setTweetText,
-  useElementDispatch,
-} from "../../Context/ElementContext";
+import { setTweetText, useElementDispatch } from "../../Context/ElementContext";
+import { toast } from "react-toastify";
+const replacer = require("react-string-replace");
 
 const TweetList = ({ data }) => {
   const classes = useStyles();
 
-  const [count, setCount] = useState(data.likes);
+  const [likes, setLikeTweet] = useState(data.likes);
   const tweetDispatch = useElementDispatch();
+  const [newlikes, setNewLikes] = useState([]);
 
-  const replacer = require("react-string-replace");
   const renderHashTag = (text) => {
     const pattern = /#(\S+)/g;
     return replacer(text, pattern, (match, i) => (
@@ -31,7 +30,50 @@ const TweetList = ({ data }) => {
   const reTweet = () => {
     setTweetText(tweetDispatch, data.text);
   };
-  
+
+  const refreshLikes = useCallback(() => {
+    fetch("https://twitterapi.liara.run/api/getAllTweet", {
+      method: "POST",
+      headers: {
+        "x-auth-token": localStorage.getItem("x-auth-token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setNewLikes(data);
+        newlikes.map((newLikes) => setLikeTweet(newLikes.likes));
+      })
+      .catch((err) => console.log(err));
+  });
+
+  useEffect(() => {
+    refreshLikes();
+  }, [refreshLikes]);
+  const uploadLike = (likes) => {
+    const config = {
+      method: "GET",
+      headers: {
+        "x-auth-token": localStorage.getItem("x-auth-token"),
+      },
+    };
+    fetch(
+      "https://twitterapi.liara.run/api/likeTweet/" + data._id,
+      config,
+      likes
+    )
+      .then((response) => {
+        if (response.statusText === "OK" && response.status === 200) {
+          refreshLikes();
+          toast.success("پست لایک شد");
+        } else toast.error("لایک پست با مشکل مواجه شد");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const likeTweet = () => {
+    setLikeTweet(likes + 1);
+    uploadLike(likes);
+  };
   return (
     <Grid container direction={"column"} className={classes.root}>
       <Grid item>
@@ -78,13 +120,8 @@ const TweetList = ({ data }) => {
         </Grid>
       </Grid>
       <Grid item className={classes.buttonContainer}>
-        <Button
-          className={classes.likeButton}
-          onClick={() => {
-            count === data.likes ? setCount(count + 1) : setCount(count);
-          }}
-        >
-          <Typography className={classes.likeCount}>{count}</Typography>
+        <Button className={classes.likeButton} onClick={likeTweet}>
+          <Typography className={classes.likeCount}>{likes}</Typography>
           <Favorite />
         </Button>
         <Button className={classes.tweetButton} onClick={reTweet}>
